690b901d03922f23140ddbdd2e7482d0dbfb4383
Antonova Maria <116485404+AntonovaMS@users.noreply.github.com>
2023-06-12 21:03:58 +0300

    Delete update_dds_dm_couriers.sql

--- update_dds_dm_couriers.sql
+++
@@ -1,6 +0,0 @@
-insert into dds.dm_couriers  (id, name, active_from) 
-select id, name, now() from stg.dm_couriers  
-where id not in (select id from dds.dm_couriers) 
-group by id,name;
-
-TRUNCATE TABLE stg.dm_couriers;
